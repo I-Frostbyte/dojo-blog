e5f5d1f19b93a622e9fbb68db7d19ad3c5d3de3b
I-Frostbyte <irefyre@gmail.com>
2022-07-14 11:09:09 +0100

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   const title = "Welcome to the new blog";
@@ -11,17 +12,18 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
+        <Home />
+        {/* <h1>{ title }</h1>
         <p>Liked { likes } times</p>
 
         {/* <p>{ person }</p> */}
 
-        <p>{ 10 }</p>
+        {/* <p>{ 10 }</p>
         <p>{ "hello ninjas" }</p>
         <p>{ [1,2,3,4,5] }</p>
         <p>{ Math.random() * 10 }</p>
 
-        <a href={ link }>Google Site</a>
+        <a href={ link }>Google Site</a> */}
       </div>
     </div>
   );
