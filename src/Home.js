f13b4f6e3ff8e11b107fd89a95adba33d1dc4254
I-Frostbyte <irefyre@gmail.com>
2022-07-20 11:05:11 +0100

    Using JSON Server

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         { title: "Web dev top tips", body: "lorem ipsum...", author: 'mario', id: 3}
     ])
 
-    const [name, setName] = useState('mario');
+    // const [name, setName] = useState('mario');
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -17,15 +17,14 @@ const Home = () => {
 
     useEffect(() => {
         console.log('use effect ran');
-        console.log(name);
-    }, [name]);
+    }, []);
     
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={ handleDelete } />
-            <button onClick={() => setName('luigi')}>Change Name</button>
-            <p>{ name }</p>
+            {/* <button onClick={() => setName('luigi')}>Change Name</button>
+            <p>{ name }</p> */}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs!" /> */}
         </div>
     );
