85107efca1437e54ce374f04928a7aec91bfff68
I-Frostbyte <irefyre@gmail.com>
2022-07-25 11:15:17 +0100

    The React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   // const title = "Welcome to the new blog";
@@ -9,12 +10,18 @@ function App() {
   // const link = "http://www.google.com";
 
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
